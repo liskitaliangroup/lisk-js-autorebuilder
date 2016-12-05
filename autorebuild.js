e54b78c0082c15bf1724c3ad4537a7cb5e8832a5
andreafspeziale <andreaf.speziale@gmail.com>
2016-12-05 10:11:00 +0100

    if node is dead it will be reloaded

--- autorebuild.js
+++ autorebuild.js
@@ -203,6 +203,13 @@ var checkReload = function() {
                 }
             }, function (err) {
                 console.log("[" + new Date().toString() + "] | " + err)
+                pauseReload = true;
+                exec.exec('bash ../lisk-test/lisk.sh reload',function (error, stdout, stderr) {
+                    console.log(stdout);
+                    if (error !== null) {
+                        console.log('exec error: ' + error);
+                    }
+                });
             });
         }, function (err) {
             console.log("[" + new Date().toString() + "] | " + err)
